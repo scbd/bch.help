5e0491a463bb7629ed86309924c3767d0ff4f374
unep-gef <unep-gef>
2008-08-18 19:52:30 +0000

    git-svn-id: https://subversion.cbd.int/svn/repo/Bch/Help@1528 ca237ed5-24d0-bc44-ad4d-3f151b82840b

---
+++ Archives/Template HelpStudio/HelpStudio 2005 Arabic Style/RegisterHelp2.bat
@@ -0,0 +1,23 @@
+REM In order to run this Help 2 registration script you must have
+REM  all COL_* files and the .hxs file in the same directory as 
+REM  this file. Additionally, InnovaHxReg.exe must be in the same
+REM  directory or in the system path.
+REM
+REM For more information on deploying Help 2 files, refer to the 
+REM  HelpStudio on-line help file under the 'Deploying the Help 
+REM  System' section.
+
+REM Register the Namespace
+InnovaHxReg /R /N /Namespace:<!--DXMETADATA start type="hhnamespace" -->namespace<!--DXMETADATA end --> /Description:"<!--DXMETADATA start type="HelpProjectTitle" -->Help Project Title<!--DXMETADATA end -->" /Collection:COL_<!--DXMETADATA start type="HelpCompilerProjectFileName" -->Help Compiler Project Filename<!--DXMETADATA end -->.hxc
+
+REM Register the help file (title in Help 2.0 terminology)
+InnovaHxReg /R /T /namespace:<!--DXMETADATA start type="hhnamespace" -->namespace<!--DXMETADATA end --> /id:<!--DXMETADATA start type="hhidentifier" -->File ID<!--DXMETADATA end --> /langid:1033 /helpfile:<!--DXMETADATA start type="ShortCHMFileName" -->Filename.hxs<!--DXMETADATA end -->
+
+REM Un-comment to plug in to the Visual Studio.NET 2002 help system
+REM InnovaHxReg /R /P /productnamespace:MS.VSCC /producthxt:_DEFAULT /namespace:<!--DXMETADATA start type="hhnamespace" -->namespace<!--DXMETADATA end --> /hxt:_DEFAULT
+
+REM Un-comment to plug in to the Visual Studio.NET 2003 help system
+REM InnovaHxReg /R /P /productnamespace:MS.VSCC.2003 /producthxt:_DEFAULT /namespace:<!--DXMETADATA start type="hhnamespace" -->namespace<!--DXMETADATA end --> /hxt:_DEFAULT
+
+REM Un-comment to plug in to the Visual Studio.NET 2005 help system
+REM InnovaHxReg /R /P /productnamespace:MS.VSIPCC.v80 /producthxt:_DEFAULT /namespace:<!--DXMETADATA start type="hhnamespace" -->namespace<!--DXMETADATA end --> /hxt:_DEFAULT
