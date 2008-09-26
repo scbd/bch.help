25de9a0f46a0f8da50c2a9ab5578c69338890ae1
unep-gef <unep-gef>
2008-09-26 22:16:53 +0000

    git-svn-id: https://subversion.cbd.int/svn/repo/Bch/Help@1801 ca237ed5-24d0-bc44-ad4d-3f151b82840b

---
+++ Archives/Docs/Project plug-in/Source/HelpStudioAddin/HelpStudioAddin/Bridge.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace HelpStudioAddin
+{
+    class Bridge
+    {
+        public void writeToXML(string sourcePath,string destinationPath)
+        {
+
+
+            //Load the source file
+            XmlTextReader myReader = new XmlTextReader(sourcePath);
+            XmlDocument mySourceDoc = new XmlDocument();
+            mySourceDoc.Load(myReader);
+            myReader.Close();
+
+
+            //Load the destination file
+            myReader = new XmlTextReader(destinationPath);
+            XmlDocument myDestDoc = new XmlDocument();
+            myDestDoc.Load(myReader);
+            myReader.Close();
+
+            //Verify if already exist the properties
+            XmlNodeList verifNodeList = myDestDoc.GetElementsByTagName("tpv");
+
+            if (verifNodeList.Count == 0) 
+            {
+                // Move from an xml to the other 
+                XmlNode rootDest = myDestDoc["HSProjectLocalization"];
+                XmlNodeList childs = mySourceDoc["Properties"].ChildNodes;
+                foreach (XmlNode nodeOrig in childs)
+                {
+
+                    XmlNode nodeDest = myDestDoc.ImportNode(nodeOrig, true);
+                    rootDest.AppendChild(nodeDest);
+                }
+
+                // Save the modified destination
+                XmlTextWriter myWriter = new XmlTextWriter(destinationPath, Encoding.UTF8);
+                myWriter.Formatting = Formatting.Indented;
+                myDestDoc.WriteTo(myWriter);
+                myWriter.Close();
+                /*Console.WriteLine("Done");
+                Console.ReadKey();*/
+
+            }
+        }
+    }
+}
---
+++ Archives/Docs/Project plug-in/Source/HelpStudioAddin/HelpStudioAddin/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelpStudioAddin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bridge xmlBridge = new Bridge();
+
+            xmlBridge.writeToXML("3082.xml", "..//project.3082.hsl");
+            xmlBridge.writeToXML("3073.xml", "..//project.3073.hsl");
+            xmlBridge.writeToXML("1049.xml", "..//project.1049.hsl");
+            xmlBridge.writeToXML("1036.xml", "..//project.1036.hsl");
+            xmlBridge.writeToXML("2052.xml", "..//project.2052.hsl");
+        }
+    }
+}
